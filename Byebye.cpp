8a3621830672c82cee014c2c8d192ff947aff4c5
Conthorcon <94369676+Conthorcon@users.noreply.github.com>
2021-11-15 21:11:18 +0700

    Add files via upload

---
+++ Byebye.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("*********************\n");
+    printf("Bye Bye World :( !\n");
+    printf("I will miss you so much\n");
+    printf("*********************");
+}
